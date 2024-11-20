6304aec6ab66a699d1b6dba2581b5eb3d66df5f2
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-20 15:51:37 +0900

    不要な記述を削除

--- app/controllers/complete_tasks_controller.rb
+++ app/controllers/complete_tasks_controller.rb
@@ -2,8 +2,4 @@ class CompleteTasksController < ApplicationController
   def index
     @completed_tasks = Task.complete.order(created_at: :desc)
   end
-
-  def show
-    @completed_task = Task.complete.find(params[:id])
-  end
 end
