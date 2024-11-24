789ad83d5d8768e6a9e594679af05c73ee715130
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-24 17:30:07 +0900

    LINEでログインできるように

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_20_041625) do
 
   create_table "users", force: :cascade do |t|
     t.string "line_user_key", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
