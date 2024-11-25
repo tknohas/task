319419dea8067594379f5e7ee666afc9f6c3fdb8
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-25 11:57:03 +0900

    ランキングの低い人がタスクにアサインされる

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,8 +1,10 @@
 class RanksController < ApplicationController
   def index
-    @user_ranks = Task.group(:executor_id)
+    @user_ranks = Task.complete
+                      .group(:executor_id)
                       .order('COUNT(executor_id) DESC')
                       .count(:executor_id)
+                      .reject { |executor_id, _| executor_id.nil? }
                       .map { |executor_id, task_count| [User.find(executor_id), task_count] }
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  before_create :assign_executor
+
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :executor, class_name: 'User', foreign_key: 'executor_id', optional: true
 
@@ -7,4 +9,18 @@ class Task < ApplicationRecord
   scope :incomplete, -> { where(completed_at: nil) }
   scope :complete, -> { where.not(completed_at: nil) }
   scope :assigned_user, -> { where.not(executor: nil) }
+
+  private
+
+  def assign_executor
+    task_counts = Task.complete.group(:executor_id).count
+    all_user_ids = User.pluck(:id)
+
+    all_user_ids.each do |user_id|
+      task_counts[user_id] ||= 0
+    end
+
+    least_assinged_user_id = task_counts.min_by { |_user_id, count| count }[0]
+    self.executor_id = least_assinged_user_id
+  end
 end
