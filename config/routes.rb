d27b7298e467426af7bfb02aa58f7e0bfc8c6d07
Masashi Takahashi <0xv17851726325v@gmail.com>
2024-11-26 22:51:29 +0900

    リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root "tasks#index"
 
-  resources :users, only: :index do
-    resource :sign_in, only: :create, module: :users
-  end
-  resource :session, only: :destroy
-  resources :passwords, param: :token
+  resource :session, only: %i[new destroy]
   resources :tasks, only: %i[index show new create edit update destroy] do
     resource :complete, only: %i[create destroy], module: :tasks
   end
@@ -13,6 +9,7 @@ Rails.application.routes.draw do
   resource :webhook, only: :create
   resources :ranks, only: :index
   resources :monthly_ranks, only: :index
+  
   get 'line_login_api/callback', to: 'line_login_api#callback'
   get 'line_login_api/login', to: 'line_login_api#login'
 end
